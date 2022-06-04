c3f866979e009d7203945a5fe62fed08cf890f72
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-06-03 19:47:46 -0500

    prev and next buttons

--- src/App.js
+++ src/App.js
@@ -16,9 +16,16 @@ function App() {
         {people.map((person, personIndex)=>{
       const {id, image, name, title, quote} = person;
       //more stuff coming up
+      let position = "nextSlide";
+      if(personIndex === index) {
+        position = "activeSlide";
+      }
+      if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+        position="lastSlide";
+      }
 
       return (
-        <article key={id}>
+        <article className={position} key={id}>
           <img src={image} alt={name} className="person-img"></img>
           <h4>{name}</h4>
           <p className="title">{title}</p>
@@ -27,10 +34,10 @@ function App() {
         </article>
       );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
