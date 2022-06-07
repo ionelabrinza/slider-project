11e53c63e70aee9abab6691eb4ac7c428bd80899
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-06-06 21:05:32 -0500

    Autoplay

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+    setIndex(lastIndex);
+  }
+   if (index > lastIndex) {
+     setIndex(0);
+   }
+  
+  }, [index, people]);
+
+  useEffect(()=> {
+    let slider = setInterval(()=> {
+      setIndex(index +1)
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return ( 
     <section className="section">
       <div className='title'>
